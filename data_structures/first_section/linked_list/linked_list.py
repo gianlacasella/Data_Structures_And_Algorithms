2ada0880837ebaec2f246944a7aa1e9c9729bdac
Gianfranco <glacasella@miuandes.cl>
2020-08-10 13:10:13 -0400

    Working on linked list

--- data_structures/first_section/linked_list/linked_list.py
+++ data_structures/first_section/linked_list/linked_list.py
@@ -42,16 +42,57 @@ class Linked_List:
             while reference.next_node != None:
                 reference = reference.next_node
             reference.next_node = Node(value, None)
+
+    def remove_head(self):
+        if self.head_node == None:
+            raise IndexError
+        else:
+            self.head_node = self.head_node.next_node
     
-    def insert_node_at_index(self, value, index):
+    def remove_tail(self):
         pass
-    
+
+    def insert_node_at_index(self, value, index):
+        if self.valid_index(index):
+            if index == 0:
+                self.add_node_at_head(value)
+            else:
+                aux_indx = 0
+                reference = self.head_node
+                while aux_indx != index-1:
+                    aux_indx += 1
+                    reference = reference.next_node
+                reference.next_node = Node(value, reference.next_node)
+        else:
+            raise IndexError
+
+    def remove_node_at_index(self, value, index):
+        pass
+
+    def valid_index(self, index):
+        if index not in range(0, self.length()):
+            return False
+        else:
+            return True
+
+    def length(self):
+        cnt = 0
+        if self.head_node == None:
+            return cnt
+        else:
+            reference = self.head_node
+            while reference != None:
+                cnt += 1
+                reference = reference.next_node
+        return cnt
 
 llist = Linked_List()
 print(llist)
+llist.add_node_at_head(1)
+print(llist)
 llist.add_node_at_head(2)
 print(llist)
-llist.add_node_at_head(3)
-print(llist)
 llist.add_node_at_tail(3)
+print(llist)
+llist.insert_node_at_index(20, 3)
 print(llist)
