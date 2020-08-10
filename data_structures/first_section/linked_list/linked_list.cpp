98f894fea4af53af598ba65debe7ad50993de936
Gianfranco <glacasella@miuandes.cl>
2020-08-10 19:01:07 -0400

    Empezando con linked list en c++

---
+++ data_structures/first_section/linked_list/linked_list.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node * next_node;
+};
+
+class Linked_List{
+    public:
+        Node * head;
+};
+
+int main() 
+{ 
+    return 0; 
+} 
